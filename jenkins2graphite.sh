30e9750125e015bac5b228719782bc1e85917ff0
Harold Spencer Jr <hspencer77@gmail.com>
2016-03-15 07:37:19 -0400

    Update jenkins2graphite.sh

--- jenkins2graphite.sh
+++ jenkins2graphite.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while [ true ];do /opt/jenkins-monitoring/jenkins-to-graphite.py --graphite-server 10.111.1.52 --jenkins-url=http://jenkins.qa1.eucalyptus-systems.com --graphite-port=2003; sleep 20;done
+while [ true ];do /opt/jenkins-monitoring/jenkins-to-graphite.py --graphite-server <graphite-server-ip> --jenkins-url=http://jenkins.qa1.eucalyptus-systems.com --graphite-port=2003; sleep 20;done
